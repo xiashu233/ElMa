95c2293bd1293d74aaefb6b91aaaf833e3cdce6d
XiaShu <2453893123@qq.com>
2019-12-01 14:04:13 +0800

    first1 commit

--- web/script/my_jquery_functions.js
+++ web/script/my_jquery_functions.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
     $("#yzm").click(function(){
-        $("#yzm").hide();
+        var phone = $("#userPhone").val();
+        var ret = /^[\d]{11}$/;
+        if (ret.test(phone)){
+            alert("手机号合法，可以进行Ajax请求");
+            $.load(
+
+            )
+
+
+        }else{
+            alert("手机号不合法");
+        }
+        // alert(phone);
+        // $("#yzm").hide();
+
     });
 });
