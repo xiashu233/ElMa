08d7f9841ec306e7711f896c87ec6558353635a5
XiaShu <2453893123@qq.com>
2019-12-04 15:43:36 +0800

    first1 commit

--- src/util/SMS.java
+++ src/util/SMS.java
@@ -38,53 +38,54 @@ public class SMS {
 
 
         // 把 mobile_code 设置为固定值 8888
-        mobile_code = 8888;
-        yzm = "1234";
+//        mobile_code = 8888;
+//        yzm = "1234";
+        yzm = mobile_code + "";
         // mobile_code 为 短信验证码
-        String content = new String("欢迎注册[六职外卖] 您的验证码是：" + mobile_code + "。请不要把验证码泄露给其他人。");
+        String content = new String("您的验证码是：" + mobile_code + "。请不要把验证码泄露给其他人。");
 
 
-//        NameValuePair[] data = {//提交短信
-//                new NameValuePair("account", "C46643721"), //查看用户名 登录用户中心->验证码通知短信>产品总览->API接口信息->APIID
-//                new NameValuePair("password", "058ee6a631dd27c06ddd9cede8c44f9a"), //查看密码 登录用户中心->验证码通知短信>产品总览->API接口信息->APIKEY
-//                //new NameValuePair("password", util.StringUtil.MD5Encode("密码")),
-//                new NameValuePair("mobile", mobile),
-//                new NameValuePair("content", content),
-//        };
-//        method.setRequestBody(data);
-//
-//        try {
-//            client.executeMethod(method);
-//
-//            String SubmitResult =method.getResponseBodyAsString();
-//
-//            //System.out.println(SubmitResult);
-//
-//            Document doc = DocumentHelper.parseText(SubmitResult);
-//            Element root = doc.getRootElement();
-//
-//            String code = root.elementText("code");
-//            String msg = root.elementText("msg");
-//            String smsid = root.elementText("smsid");
-//
-//            System.out.println(code);
-//            System.out.println(msg);
-//            System.out.println(smsid);
-//
-//            if("2".equals(code)){
-//                System.out.println("短信提交成功");
-//            }
-//
-//        } catch (HttpException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (DocumentException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
+        NameValuePair[] data = {//提交短信
+                new NameValuePair("account", "C46643721"), //查看用户名 登录用户中心->验证码通知短信>产品总览->API接口信息->APIID
+                new NameValuePair("password", "058ee6a631dd27c06ddd9cede8c44f9a"), //查看密码 登录用户中心->验证码通知短信>产品总览->API接口信息->APIKEY
+                //new NameValuePair("password", util.StringUtil.MD5Encode("密码")),
+                new NameValuePair("mobile", mobile),
+                new NameValuePair("content", content),
+        };
+        method.setRequestBody(data);
+
+        try {
+            client.executeMethod(method);
+
+            String SubmitResult =method.getResponseBodyAsString();
+
+            //System.out.println(SubmitResult);
+
+            Document doc = DocumentHelper.parseText(SubmitResult);
+            Element root = doc.getRootElement();
+
+            String code = root.elementText("code");
+            String msg = root.elementText("msg");
+            String smsid = root.elementText("smsid");
+
+            System.out.println(code);
+            System.out.println(msg);
+            System.out.println(smsid);
+
+            if("2".equals(code)){
+                System.out.println("短信提交成功");
+            }
+
+        } catch (HttpException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
 }
